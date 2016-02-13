52e268e0f434a795117d60570b628530f79bae79
Anthony Dario <anthonyrdario@gmail.com>
2016-02-13 15:12:10 -0500

    Checking for 'not no'
    Also moving id's into variables

--- server.py
+++ server.py
@@ -8,23 +8,35 @@ import datetime
 class GroupRequestHandler(BaseHTTPRequestHandler):
 
 	def do_POST(self):
+
+        dario_bot = '9af5b2ffe7a277e4f9f108f8f7'
 		post_url = 'https://api.groupme.com/v3/bots/post'
+
 		length = int(self.headers['Content-Length'])
 		request = self.rfile.read(length)
 		json_request = json.loads(request)
-		if json_request['sender_type'] == 'user' and \
-		   json_request['text'].lower().strip().replace(".", "") == 'spring break':
 
-			today = datetime.datetime.today()
-			rd = relativedelta(datetime.datetime(2016, 3, 5, 0, 0, 0, 0), today)
-			response = { }
-			response['text'] = str(rd.months)  + ' months ' + \
-                               str(rd.days)    + ' days ' + \
-                               str(rd.hours)   + ' hours ' + \
-                               str(rd.minutes) + ' minutes ' + \
-                               str(rd.seconds) + ' seconds'
-			response['bot_id'] = '9af5b2ffe7a277e4f9f108f8f7'
-			requests.post(post_url, data=response)
+		if json_request['sender_type'] == 'user':
+
+		    if json_request['text'].lower().strip().replace(".", "") == 'spring break':
+
+                today = datetime.datetime.today()
+                rd = relativedelta(datetime.datetime(2016, 3, 5, 0, 0, 0, 0), today)
+                response = { }
+                response['text'] = str(rd.months)  + ' months ' + \
+                                   str(rd.days)    + ' days ' + \
+                                   str(rd.hours)   + ' hours ' + \
+                                   str(rd.minutes) + ' minutes ' + \
+                                   str(rd.seconds) + ' seconds'
+                response['bot_id'] = dario_bot 
+                requests.post(post_url, data=response)
+
+            if re.search("not no", json_request['text'], flags = 0):
+                response = { }
+                response['text'] = "not no = yes"
+                response['bot_id'] = dario_bot
+                requests.post(post_url, data=response)
+
 
 # hosting the server
 HandlerClass = SimpleHTTPRequestHandler
